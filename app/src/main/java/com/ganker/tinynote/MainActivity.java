6d58505b48de396f65b5c851cdd19e89dcf39e9d
Jackson Liu <416883813@qq.com>
2016-03-11 23:39:31 +0800

    add left-top button for drawer open/close

--- app/src/main/java/com/ganker/tinynote/MainActivity.java
+++ app/src/main/java/com/ganker/tinynote/MainActivity.java
@@ -3,17 +3,24 @@ package com.ganker.tinynote;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.ganker.tinynote.frament.MainActivityFragment;
 import com.ganker.tinynote.frament.NoteFragment;
 
 public class MainActivity extends AppCompatActivity implements MainActivityFragment.OnFragmentInteractionListener,NoteFragment.OnFragmentInteractionListener{
     private final String TAG = "MainActivity";
+
+    private DrawerLayout mdrawerlayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,29 @@ public class MainActivity extends AppCompatActivity implements MainActivityFragm
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.notes_title);
         setSupportActionBar(toolbar);
+        //给左上角加上一个返回图标
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //设定左上角图标可以点击
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mdrawerlayout = (DrawerLayout) findViewById(R.id.drawerlayout_left);
+        //菜单出现和消失时绑定一些事件,点击按钮，出现/关闭菜单
+        mDrawerToggle = new ActionBarDrawerToggle(this, mdrawerlayout, toolbar,R.string.open_menu,R.string.close_menu){
+            @Override
+            public void onDrawerOpened(View drawerview) {
+                super.onDrawerOpened(drawerview);
+                invalidateOptionsMenu();
+            }
+            @Override
+            public void onDrawerClosed(View drawerview) {
+                super.onDrawerClosed(drawerview);
+                invalidateOptionsMenu();
+            }
+
+        };
+        mDrawerToggle.syncState();
+        //绑定菜单事件
+        mdrawerlayout.setDrawerListener(mDrawerToggle);
 
         if(savedInstanceState == null) {
             Log.d(TAG, "call fragment");
